f988619e1c005017fae50c6b105f51e101a10ff2
gyankov <g.yankov951@abv.bg>
2016-09-01 11:13:02 +0300

    Variables, Data, Expressions homework

---
+++ Variables, Data, Expressions and Constants Homework/VariablesDataExpressions/VariablesDataExpressions/ClassSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Size
+{
+    private double width, height;
+
+    public Size(double w, double h)
+    {
+        this.width = w;
+        this.height = h;
+    }
+
+    public static Size GetRotatedSize(
+        Size s,
+        double angleOfTheFigureThatWillBeRotaed)
+    {
+        var width = (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.width) +
+               (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.height);
+        var height = (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * s.width) +
+                (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * s.height);
+        var size = new Size(width, height);
+
+        return size;
+    }
+}
---
+++ Variables, Data, Expressions and Constants Homework/VariablesDataExpressions/VariablesDataExpressions/MethodPrintStatistics.cs
@@ -0,0 +1,55 @@
+﻿namespace VariablesDataExpressions
+{
+    using System;
+
+    public class MethodPrintStatistics
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double maxValue, tmp; 
+            tmp = 0;
+            maxValue = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > maxValue)
+                {
+                    maxValue = arr[i];
+                }
+            }
+
+            PrintMax(maxValue);
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < maxValue)
+                {
+                    maxValue = arr[i];
+                }
+            }
+
+            PrintMin(maxValue);
+
+            tmp = 0;
+            for (int i = 0; i < count; i++)
+            {
+                tmp += arr[i];
+            }
+
+            PrintAvg(tmp / count);
+        }
+
+        private void PrintMax(double max)
+        {
+            Console.WriteLine(max);
+        }
+
+        private void PrintMin(double min)
+        {
+            Console.WriteLine(min);
+        }
+
+        private void PrintAvg(double avg)
+        {
+            Console.WriteLine(avg);
+        }
+    }
+}
